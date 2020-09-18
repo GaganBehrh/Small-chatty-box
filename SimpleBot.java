136a62f6034990a8521cb0e733729b26600663b2
GaganBehrh <63474017+GaganBehrh@users.noreply.github.com>
2020-09-18 16:30:03 +0200

    Add files via upload

--- SimpleBot.java
+++ SimpleBot.java
@@ -46,20 +46,20 @@ public class SimpleBot {
     static void test() {
         System.out.println("Let's test your programming knowledge.");
         System.out.println("Why do we use methods?");
-     System.out.println("1.To repeat a statement multiple times.");
-     System.out.println("2.To decompose a program into several small subroutines.");
-     System.out.println("3.To determine the execution time of a program.");
-     System.out.println("4.To interrupt the execution of a program.");
-     Scanner input= new Scanner(System.in);
-     int choice= input.nextInt();
-     if (choice==1 || choice ==2 || choice==3)
-     {
-         end();
-     }
-     else
-     {
-         System.out.println("Please,try again.");
-     }
+        System.out.println("1.To repeat a statement multiple times.");
+        System.out.println("2.To decompose a program into several small subroutines.");
+        System.out.println("3.To determine the execution time of a program.");
+        System.out.println("4.To interrupt the execution of a program.");
+        Scanner input= new Scanner(System.in);
+        int choice= input.nextInt();
+        if (choice==1 || choice ==2 || choice==3)
+        {
+            end();
+        }
+        else
+        {
+            System.out.println("Please,try again.");
+        }
 
     }
 
